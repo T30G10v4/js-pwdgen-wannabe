835796aa3ed7350a5d230431fe0cc2156ed1c89d
T30G10v4 <matteo.giovagnotti@gmail.com>
2022-10-05 13:27:27 +0200

    Test con controlli

--- js/script.js
+++ js/script.js
@@ -5,11 +5,18 @@
 
 // INPUT
 const userName = prompt("Inserisci il tuo nome");
+console.log(userName, typeof(userName));
+
 const userSurname = prompt("Inserisci il tuo cognome");
+console.log(userSurname, typeof(userSurname));
+
 const userColor = prompt("Inserisci il tuo colore preferito");
+console.log(userColor, typeof(userColor));
 
 // ELABORAZIONE
 const result = `${userName}${userSurname}${userColor}21`;
+console.log(result, typeof(result));
 
 // OUTPUT
 document.getElementById("password").innerHTML = result;
+console.log(result, typeof(result));
